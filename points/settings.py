031fe0f4d2603a0e176c27f94bbf0d208e68254b
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-10-19 22:49:48 +0530

    preview

--- points/settings.py
+++ points/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,4 +135,8 @@ LOGOUT_REDIRECT_URL = '/'  # Replace with the URL of your login page
   
 
 CSRF_TRUSTED_ORIGINS = ['https://fe9a-103-159-151-109.ngrok-free.app']
+
+# Media URL and Root
+MEDIA_URL = ''
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
